fc4db2fbe1deec779192dfa1842145b4a38e49ba
Matheus A Martins <matheusthebr@gmail.com>
2021-01-20 16:01:08 -0300

    Setup rspec config.

--- lib/stalker/engine.rb
+++ lib/stalker/engine.rb
@@ -2,5 +2,9 @@ module Stalker
   class Engine < ::Rails::Engine
     isolate_namespace Stalker
     config.generators.api_only = true
+
+    config.generators do |generators|
+      generators.test_framework :rspec
+    end
   end
 end
